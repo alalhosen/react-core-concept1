514b0bffc7e2088d1840974cd45cfc0a5d4862cd
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-31 20:31:50 +0600

    Navber create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Countries from './Component/Countries'
+import Navber from './Component/Navber/Navber'
 
 function App() {
 
   return (
     <>
+      <Navber></Navber>
       <h1>React World Tour</h1>
       <Countries></Countries>
     </>
---
+++ src/Component/Navber/Navber.jsx
@@ -0,0 +1,14 @@
+const Navber = () => {
+    return (
+        <div className="flex display-flex">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navber;
